81256ffd85d92cfc2286d4f46a43c51688ecc8ab
ProgSaleh <progsaleh@hotmail.com>
2023-10-07 01:56:46 +0300

    setup prisma

---
+++ lib/prismadb.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+debugger;
+
+// if you do this:
+//    const prismadb = new PrismaClient();
+// in developement env,
+// you'll create a new PrismaClient EVERY SINGLE TIME you change in the code
+// (hot reloading will run and reinstanciate the client!!)
+const prismadb = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismadb;
+
+export default prismadb;
