10081968b05c5c2ffc940f9df1cd2297cd42a8cd
ProgSaleh <progsaleh@hotmail.com>
2023-10-08 05:12:18 +0300

    handle colors

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/client.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useRouter, useParams } from 'next/navigation';
+import { Billboard } from '@prisma/client';
+
+import { Heading } from '@/components/ui/heading';
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+import { ColorColumn, columns } from './columns';
+import { Separator } from '@/components/ui/separator';
+import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
+
+interface ColorsClientProps {
+  data: ColorColumn[];
+}
+
+export const ColorsClient: React.FC<ColorsClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors(${data.length})`}
+          description="Manage colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="calls for colors" />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
+    </>
+  );
+};
